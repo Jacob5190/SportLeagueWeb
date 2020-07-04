58d52432b14ed76adef971a1b08d2bb5b4f36da3
BuildTools <unconfigured@null.spigotmc.org>
2020-07-05 03:35:03 +0800

    2020/7/5 First update Continue testing with Shiro

---
+++ src/main/java/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package serviceImpl;
+
+import mapper.user;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pojo.User;
+import service.userService;
+
+@Service
+public class UserServiceImpl implements userService {
+	@Autowired
+	user user;
+	@Override
+	public String getPassword (String name) {
+		User u = user.selectUser(name);
+		if (null == u){
+			return null;
+		}
+		return u.getPassword();
+	}
+}
